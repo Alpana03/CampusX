28e1d1a470d2b88b26f31b9a3c2a983af932d487
Alpana03 <alpana.103110@gmail.com>
2026-06-22 23:29:44 +0530

    Create login_usingdecisionmaking.py

---
+++ login_usingdecisionmaking.py
@@ -0,0 +1,6 @@
+Username = input("Please enter Username: ")
+Password = int(input("Please enter Password: "))
+if (Username == "xyz@gmail.com" and Password == 1234):
+    print("Welcome")
+else:
+    print("Login failed")
